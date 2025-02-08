9407c6e253ad243c6e4c9bccbd2e3638c36d287d
anonymByte-404 <an0nymnx@outlook.com>
2025-02-07 23:39:10 -0500

    feat: add header with navigation links (Blog, Projects, Resume)

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Header: React.FC = () => {
+  return (
+    <header>
+      <h1>Aeron Asug</h1>
+      <nav className='nav-links'>
+        <ul>
+          <li><a href="/blog" aria-label="Blog">Blog</a></li>
+          <li><a href="/projects" aria-label="Projects">Projects</a></li>
+          <li><a href="/resume" aria-label="Resume">Resume</a></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
