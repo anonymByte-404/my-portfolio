c5010565405c7c7287410f6269ba78ba697c1410
anonymByte-404 <an0nymnx@outlook.com>
2025-02-08 21:18:13 -0500

    feat: add Skill section to showcase technologies I work with

---
+++ src/components/Skill.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Skill: React.FC = () => {
+  return (
+    <section id="skill">
+      <div className="legend">
+        <h2>What I Work With</h2>
+      </div>
+    </section>
+  )
+}
