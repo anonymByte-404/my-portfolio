d686a69bf419e6a2a44f10957a731f551df9f980
anonymByte-404 <an0nymnx@outlook.com>
2025-02-16 22:33:51 -0500

    feat: add Blog component with Header integration

---
+++ src/pages/Blog.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { Header } from "components/Header"
+
+export const Blog: React.FC = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>Blog</h1>
+      </main>
+    </div>
+  )
+}
