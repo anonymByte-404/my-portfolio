902079ec489b43528d99288ba2c2157642925e15
Aeron Asug <an0nymnx@outlook.com>
2025-03-03 01:16:22 +0000

    feat: add About component placeholder for future improvements

---
+++ frontend/src/ui/pages/About.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About: React.FC = () => {
+    return (
+        <section id='about' className='about'>
+            <h2>About</h2>
+        </section>
+    )
+}
+
+export default About
