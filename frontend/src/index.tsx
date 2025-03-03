068eae27b4c8acd371b6590430767cdeae4ffb5e
Aeron Asug <an0nymnx@outlook.com>
2025-03-03 01:14:15 +0000

    chore: remove reportWebVitals from index.ts

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import reportWebVitals from './reportWebVitals'
 import './styles/main.css'
 
 const root = ReactDOM.createRoot(
@@ -13,6 +12,3 @@ root.render(
   </React.StrictMode>
 )
 
-reportWebVitals((metric) => {
-  console.log(metric)
-})
