6c619ba7e2d828417007da977dd744806434497a
Aeron Asug <an0nymnx@outlook.com>
2025-04-01 21:35:09 -0600

    feat(pages): add new TSX files (About, Blog, Home, Projects) for routing

---
+++ frontend/src/gui/pages/About.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App: React.FC<{}> = () => {
+  return (
+    <section className='about-section'>
+      <h1>About</h1>
+    </section>
+  )
+}
+
+export default App
