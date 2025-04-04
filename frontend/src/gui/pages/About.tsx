93774a5cfd783122e90c75079f241f4286353ea5
Aeron Asug <an0nymnx@outlook.com>
2025-04-03 22:34:54 -0600

    feat: update About section with personal introduction and skills

--- frontend/src/gui/pages/About.tsx
+++ frontend/src/gui/pages/About.tsx
@@ -3,7 +3,19 @@ import React from 'react'
 const App: React.FC<{}> = () => {
   return (
     <section className='about-section'>
-      <h1>About</h1>
+      <div className='about-me'>
+        <span className='about-header'>MORE ABOUT ME</span>
+        <h1 className='greeting'>
+          Hi there! I'm <span className='name'>Aeron</span>
+        </h1>
+        <p className='about-description'>
+          I'm Aeron Asug, a 15-year-old aspiring software engineer who started programming a couple of months ago.
+          Although I don't have much time to dedicate to learning because of school, I'm passionate about growing
+          my skills in software engineering, including languages like C/C++, Python, and Ruby. I'm also exploring
+          useful tools and technologies such as React, Next.js, and Node.js, and I have a strong interest in learning
+          about cybersecurity.
+        </p>
+      </div>
     </section>
   )
 }
