ad88bafeaaae1ed1ee4f8c837b861682b9b9b24e
Aeron Asug <an0nymnx@outlook.com>
2025-04-04 23:12:27 -0600

    feat(icons): add Arrow and Download icons

--- frontend/src/gui/components/Icons.tsx
+++ frontend/src/gui/components/Icons.tsx
@@ -4,6 +4,18 @@ interface IconProps {
   size?: number
 }
 
+export const Arrow: React.FC<IconProps> = ({ size }) => {
+  return (
+    <svg
+      viewBox='0 0 512 512'
+      width={size}
+      height={size}
+    >
+      <path d='M322.7,128.4L423,233.4c6,5.8,9,13.7,9,22.4c0,8.7-3,16.5-9,22.4L322.7,383.6c-11.9,12.5-31.3,12.5-43.2,0c-11.9-12.5-11.9-32.7,0-45.2l48.2-50.4h-217C93.7,288,80,273.7,80,256c0-17.7,13.7-32,30.6-32h217l-48.2-50.4c-11.9-12.5-11.9-32.7,0-45.2C291.4,115.9,310.7,115.9,322.7,128.4z' />
+    </svg>
+  )
+}
+
 export const Github: React.FC<IconProps> = ({ size }) => {
   return (
     <svg
@@ -20,6 +32,27 @@ export const Github: React.FC<IconProps> = ({ size }) => {
     </svg>
   )
 }
+
+export const Download: React.FC<IconProps> = ({ size }) => {
+  return (
+    <svg
+      stroke-linejoin="round"
+      stroke-linecap="round"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="1.5"
+      viewBox="0 0 24 24"
+      height={size}
+      width={size}
+    >
+      <path fill="none" d="M0 0h24v24H0z" stroke="none"></path>
+      <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2"></path>
+      <path d="M7 11l5 5l5 -5"></path>
+      <path d="M12 4l0 12"></path>
+    </svg>
+  )
+}
+
 export const Mail: React.FC<IconProps> = ({ size }) => {
   return (
     <svg
@@ -53,5 +86,5 @@ export const Mail: React.FC<IconProps> = ({ size }) => {
         </g>
       </g>
     </svg>
-  );
-};
+  )
+}
