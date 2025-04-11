a7feaf1b086acf91763db4cbf465523bc61bdf8a
Aeron Asug <an0nymnx@outlook.com>
2025-04-10 22:28:04 -0600

    refactor: clean up App component structure and enhance route components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,58 +1,84 @@
 import React, { JSX } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { Components, Pages } from './gui/index'
+import { Images } from './assets'
 import './styles/css/index.min.css'
 
-const App: React.FC<{}> = () => {
-  const routes: {
-    path: string
-    label: string
-    component: React.FC<{}>
-  }[] = [
-      { path: '/', label: 'Home', component: Pages.Home },
-      { path: '/about', label: 'About', component: Pages.About },
-      { path: '/projects', label: 'Projects', component: Pages.Projects },
-      { path: '/blog', label: 'Blog', component: Pages.Blog }
-    ]
+interface SocialLink {
+  icon: JSX.Element
+  link: string
+  label: string
+}
 
-  const socialLinks: {
-    icon: JSX.Element
-    link: string
-    label: string
-    size: number
-  }[] = [
-      { icon: <Components.Icons._Github_ />, link: 'https://github.com/anonymByte-404', label: 'GitHub', size: 30 }
-    ]
+type RouteItem = {
+  path: string
+  label: string
+  component: React.FC | (() => JSX.Element)
+}
 
-  const email: {
-    icon: React.ReactNode
-    address: string
-    size?: number
-  } = {
+const App: React.FC = () => {
+  const socialLinks: SocialLink[] = [
+    {
+      icon: <Components.Icons._Github_ />,
+      link: 'https://github.com/anonymByte-404',
+      label: 'GitHub'
+    }
+  ]
+
+  const email = {
     icon: <Components.Icons._Mail_ />,
-    address: 'fake@email.com',
-    size: 30
+    address: 'fake@email.com'
   }
 
-  const techLinks: {
-    name: string
-    href: string
-    icon: JSX.Element
-  }[] = [
-      { name: 'TypeScript', href: 'https://www.typescriptlang.org/', icon: <Components.Icons._TypeScript_ size={20} /> },
-      { name: 'React', href: 'https://react.dev/', icon: <Components.Icons._React_ size={20} /> },
-      { name: 'SCSS', href: 'https://sass-lang.com/', icon: <Components.Icons._Sass_ size={20} /> },
-    ]
+  const techLinks = [
+    {
+      name: 'TypeScript',
+      href: 'https://www.typescriptlang.org/',
+      icon: <Components.Icons._TypeScript_ size={20} />
+    },
+    {
+      name: 'React',
+      href: 'https://react.dev/',
+      icon: <Components.Icons._React_ size={20} />
+    },
+    {
+      name: 'SCSS',
+      href: 'https://sass-lang.com/',
+      icon: <Components.Icons._Sass_ size={20} />
+    }
+  ]
+
+  const routes: RouteItem[] = [
+    { path: '/', label: 'Home', component: Pages.Home },
+    {
+      path: '/about',
+      label: 'About',
+      component: () => <Pages.About socialLinks={socialLinks} />
+    },
+    {
+      path: '/projects',
+      label: 'Projects',
+      component: () => <Pages.Projects placeholder={Images.placeholder} />
+    },
+    { path: '/blog', label: 'Blog', component: Pages.Blog }
+  ]
 
   return (
     <>
-      <Components.Header links={routes} icon={<Components.Icons._Download_ size={20} />} />
+      <Components.Header
+        links={routes}
+        icon={<Components.Icons._Download_ size={20} />}
+      />
       <Routes>
         {routes.map(({ path, component: Component }) => (
           <Route key={path} path={path} element={<Component />} />
         ))}
       </Routes>
-      <Components.Footer socialLinks={socialLinks} email={email} techLinks={techLinks} />
+      <Components.Footer
+        socialLinks={socialLinks}
+        email={email}
+        techLinks={techLinks}
+      />
     </>
   )
 }
